267b4aaab2a8e9785feb2a47c87279e6a1393313
caiwei <caiwei@stanford.edu>
2024-02-20 20:32:33 -0800

    Define DisNet_BASE class

---
+++ python/base_classes/disnet_base.py
@@ -0,0 +1,121 @@
+"""@package docstring
+DisNet_BASE: base class for DisNet (dislocation network)
+
+Defines interface for the DisNet class
+"""
+
+from abc import ABC, abstractmethod 
+
+class DisNet_BASE(ABC):
+    """DisNet_BASE: base class for DisNet (dislocation network)
+
+    Defines interface for the DisNet class
+    """
+    @abstractmethod
+    def nodes(self):
+        """nodes: return list of all node tags (keys)
+        """
+        pass
+    
+    @abstractmethod
+    def nodes(self, tag):
+        """nodes: return property of node specified by tag (key)
+        """
+        pass
+
+    @abstractmethod
+    def segments(self):
+        """segments: return list of all segments (tag pairs)
+        """
+        pass
+
+    @abstractmethod
+    def segments(self, tag_pair):
+        """segments: return property of segments specified by tag_pair
+        """
+        pass
+    
+    @abstractmethod
+    def out_degree(self, tag):
+        """out_degree: return out degree of a node specified by tag (key)
+        """
+        pass
+
+    @abstractmethod
+    def node_prop_list(self):
+        """node_pos_list: return a list of node properties
+           (each item of the list is a dictionary)
+        """
+        pass
+
+    @abstractmethod
+    def seg_prop_list(self):
+        """seg_prop_list: return a numpy array of segment properties
+           (each item of the list is a dictionary)
+        """
+        pass
+    
+    @abstractmethod
+    def has_node(self, tag) -> bool:
+        """has_node: check if a node exists in the network
+        """
+        pass
+    
+    @abstractmethod
+    def has_segment(self, tag1, tag2) -> bool:
+        """has_edge: check if an edge exists in the network
+        """
+        pass
+    
+    @abstractmethod
+    def add_nodes_segments_from_list(self, rn, links) -> None:
+        """add_nodes_links_from_list: add nodes and edges stored in lists to network
+        """
+        pass
+    
+    @abstractmethod
+    def insert_node(self, tag1, tag2, tag):
+        """insert_node: insert a node (tag) between two existing nodes (tag1 and tag2)
+        """
+        pass
+    
+    @abstractmethod
+    def remove_two_arm_node(self, tag):
+        """remove_two_arm_node: remove a node with two arms from the network
+        """
+        pass
+
+    @abstractmethod 
+    def merge_node(self, tag1, tag2):
+        """merge_node: merge two nodes into one
+           return mergedTag (tag1 or tag2) if merge is successful, None otherwise
+
+        Remove any links between node1 and node2
+        If merged node has double-links to any neighbor, combine them into one (or zero) link
+        """
+        pass
+    
+    @abstractmethod
+    def split_node(self, tag, nbrs_to_split):
+        """split_node: split a node into two nodes with neighbors split according to partition
+
+        inputs:
+           tag: tag of the node to be split
+           nbrs_to_split: list of tags of neighbors to be split off to the new node
+
+        outputs:
+           split_node1: tag of the node to which all unselected arms are connected
+           split_node2: tag of the node to which all selected arms are connected
+        """
+        pass
+
+    @abstractmethod
+    def is_sane(self) -> bool:
+        """is_sane: check if the network is sane
+
+        The two arms connecting two nodes should have opposite Burgers vectors and parallel plane_normal vectors
+        The sum of all Burgers vectors of outgoing arms from a node should be zero
+        """
+        pass
+    
+    # perhaps split_multi_nodes should belong to another (higher level) module
