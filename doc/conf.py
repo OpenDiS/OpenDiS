b0240a0e641fafa06cba5fb994761b98219f21b1
ekrakov <144193466+ekrakov@users.noreply.github.com>
2024-05-08 21:31:50 -0700

    Update conf.py
    change welcome

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme_options = {
-    "announcement": "<em>Important</em> Welcome to Open Dis :) ",
+    "announcement": "<em>Welcome to Open Dis :)</em>  ",
 }
 
 # change theme color 
