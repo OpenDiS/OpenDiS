5f1dcfb9d37b028ffe32e3f711806d65a106b95d
ekrakov <144193466+ekrakov@users.noreply.github.com>
2024-05-09 00:38:20 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,7 @@ html_theme_options = {
     "light_css_variables": {
         "color-brand-primary": "#7C4DFF",
         "color-brand-content": "#7C4DFF",
-        "color-background-secondary": "#f7ebf7",
+        "color-background-secondary": "#fff7ef",
     },
 }
 
