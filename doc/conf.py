e40b406bdfeed6bc0ddadf4fb7aa946fe2c8c4f9
Eliana Krakovsky <144193466+ekrakov@users.noreply.github.com>
2024-05-10 21:54:06 -0700

    Update conf.py
    add temp favicon to test. Will delete later.

--- doc/conf.py
+++ doc/conf.py
@@ -66,5 +66,5 @@ html_theme_options = {
 html_logo = "box_logo_upright.png"
 html_theme =  "furo"
 html_static_path = ['_static']
-favicons = ['box_logo_upright.png']
-html_favicons = ['box_logo_upright.png']
+favicons = ['favicon.png']
+html_favicons = ['favicon.png']
