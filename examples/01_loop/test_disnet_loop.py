8d282079ceb565a6bf0873d0fd35bf9526264b0e
Hanfeng Zhai <hzhai@stanford.edu>
2024-05-31 16:02:41 -0700

    add loop test case

--- examples/01_loop/test_disnet_loop.py
+++ examples/01_loop/test_disnet_loop.py
@@ -1,25 +1,30 @@
 import numpy as np
 import sys, os
 
-sys.path.extend([os.path.abspath('../../python'),os.path.abspath('../../lib')])
+sys.path.extend([os.path.abspath('../../python'),os.path.abspath('../../lib'),os.path.abspath('../../core/pydis/python')])
 
-from pydis.disnet import DisNet
+from framework.disnet_manager import DisNetManager
+from pydis.disnet import DisNode, Cell, CellList, DisNet
 from pydis.calforce.calforce_disnet import CalForce
 from pydis.mobility.mobility_disnet import MobilityLaw
 from pydis.timeint.timeint_disnet import TimeIntegration
+from pydis.topology.topology_disnet import Topology
 from pydis.collision.collision_disnet import Collision
 from pydis.remesh.remesh_disnet import Remesh
 from pydis.visualize.vis_disnet import VisualizeNetwork
 from pydis.simulate.sim_disnet import SimulateNetwork
 
-def init_circular_loop(radius=1.0, N=20, burg_vec=np.array([1.0,0.0,0.0])):
+def init_circular_loop(radius=1.0, N=20, burg_vec=np.array([1.0,0.0,0.0]),pbc=False):
     print("init_circular_loop: radius = %f, N = %d" % (radius, N))
-    G = DisNet()
+    G = DisNetManager()
     theta = np.arange(N)*2.0*np.pi/N
     rn    = np.vstack([radius*np.cos(theta), radius*np.sin(theta), np.zeros_like(theta)]).T
     links = np.zeros((N, 5))
     for i in range(N):
         links[i,:] = np.array([i, (i+1)%N, burg_vec[0], burg_vec[1], burg_vec[2]])
+    cell = Cell(h=8.0*np.eye(3), is_periodic=[True,True,True])
+    cell_list = CellList(cell=cell, n_div=[8,8,8])
+    G.add_disnet(cell=cell, cell_list=cell_list)
     G.add_nodes_links_from_list(rn, links)
     return G
 
@@ -27,7 +32,7 @@ def main():
     global G, sim
     G = init_circular_loop()
 
-    bounds = np.array([[-2.0, -2.0, -2.0], [2.0, 2.0, 2.0]])
+    bounds = np.array([-0.5*np.diag(G.cell.h), 0.5*np.diag(G.cell.h)])
     vis = VisualizeNetwork(bounds=bounds)
     # for debugging purposes
     #vis.plot_disnet(G)
@@ -43,11 +48,12 @@ def main():
 
     mobility  = MobilityLaw(mobility_law='Relax')
     timeint   = TimeIntegration(integrator='EulerForward')
+    topology  = Topology(split_mode='MaxDiss')
     collision = None #Collision(collision_mode='Proximity')
     remesh    = None #Remesh(remesh_rule='LengthBased')
 
     sim = SimulateNetwork(calforce=calforce, mobility=mobility,
-                          timeint=timeint, collision=collision, remesh=remesh, vis=vis,
+                          timeint=timeint, topology=topology, collision=collision, remesh=remesh, vis=vis,
                           dt0 = 1.0e-8, max_step=200, 
                           print_freq=10, plot_freq=10, plot_pause_seconds=0.1)
     sim.run(G)
