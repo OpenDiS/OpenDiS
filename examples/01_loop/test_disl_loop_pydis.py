55238b616c4f1e92d04ef62d96add098756b9103
Hanfeng Zhai <hzhai@stanford.edu>
2024-06-04 10:20:18 -0700

    add GNN mobility law

--- examples/01_loop/test_disl_loop_pydis.py
+++ examples/01_loop/test_disl_loop_pydis.py
@@ -18,58 +18,44 @@ def init_circular_loop(radius=1.0, N=20, burg_vec=np.array([1.0,0.0,0.0]),pbc=Fa
     print("init_circular_loop: radius = %f, N = %d" % (radius, N))
     theta = np.arange(N)*2.0*np.pi/N
     rn    = np.vstack([radius*np.cos(theta), radius*np.sin(theta), np.zeros_like(theta)]).T
-    links = np.zeros((N, 5))
+    links = np.zeros((N, 8))
     for i in range(N):
-        links[i,:] = np.array([i, (i+1)%N, burg_vec[0], burg_vec[1], burg_vec[2]])
+        pn = np.cross(burg_vec, rn[(i+1)%N,:3]-rn[i,:3])
+        pn = pn / np.linalg.norm(pn)
+        links[i,:] = np.concatenate(([i, (i+1)%N], burg_vec, pn))
     cell = Cell(h=8.0*np.eye(3), is_periodic=[True,True,True])
     cell_list = CellList(cell=cell, n_div=[8,8,8])
     G = DisNetManager(DisNet(cell=cell, cell_list=cell_list, rn=rn, links=links))
     return G
 
 def main():
-    global G, sim
-    G = init_circular_loop()
+    global net, sim
+    net = init_circular_loop(pbc=True)
 
-    bounds = np.array([-0.5*np.diag(G.cell.h), 0.5*np.diag(G.cell.h)])
+    bounds = np.array([-0.5*np.diag(net.cell.h), 0.5*np.diag(net.cell.h)])
     vis = VisualizeNetwork(bounds=bounds)
-    # for debugging purposes
-    #vis.plot_disnet(G)
 
+    # "burgmag" not yet used in pydis, make parameters (Ec) more physical
     params = {"burgmag": 3e-10, "mu": 160e9, "nu": 0.31, "a": 0.01, "maxseg": 0.3, "minseg": 0.1, "rann": 0.0316}
     calforce = CalForce(force_mode='LineTension', params=params, Ec=1.0e6)
-    applied_stress=np.array([0.0, 0.0, 0.0, 0.0, 1.0e5, 0.0])
-    nodeforce_dict = calforce.NodeForce(G, applied_stress=applied_stress)
-    # lt_force_array = np.array([nodeforce_dict[node] for node in G.nodes])
-    node_force = [array for array in nodeforce_dict[0].values()] # force at nodes
-    seg_force = [array for array in nodeforce_dict[1].values()]
-    # print(np.array(a1), np.array(a2))
-    # for debugging purposes
-    # nodeforce_dict = calforce.NodeForce(G)
-    # lt_force_array = np.array([nodeforce_dict[node] for node in G.nodes])
-    # np.savetxt("force.dat", lt_force_array)
-    # print("save force to 'force.dat'")
-
-    mobility  = MobilityLaw(mobility_law='Relax')
-    graph = G.get_disnet()
-    node_vel = mobility.Mobility_Relax(graph, nodeforce_dict=node_force)
-    timeint   = TimeIntegration(integrator='EulerForward')
-    topology  = Topology(split_mode='MaxDiss')
-    collision = None #Collision(collision_mode='Proximity')
-    remesh    = None #Remesh(remesh_rule='LengthBased')
+    mobility  = MobilityLaw(mobility_law='GNN', params=params)#SimpleGlide
+    timeint   = TimeIntegration(integrator='EulerForward', dt=1.0e-9, params=params)
+    topology  = Topology(split_mode='MaxDiss', params=params)
+    collision = None#Collision(collision_mode='Proximity', params=params)
+    remesh    = None#Remesh(remesh_rule='LengthBased', params=params)
 
     sim = SimulateNetwork(calforce=calforce, mobility=mobility, timeint=timeint,
                           topology=topology, collision=collision, remesh=remesh, vis=vis,
-                          max_step=200, loading_mode="stress",
-                          applied_stress=applied_stress,
+                          max_step=100, loading_mode="stress",
+                          applied_stress=np.array([0.0, 0.0, 0.0, 0.0, 1.0e5, 0.0]),
                           print_freq=10, plot_freq=10, plot_pause_seconds=0.1,
                           write_freq=10, write_dir='output')
-    
-    sim.run(G)
-    
-    return G.is_sane()
+    sim.run(net)
+
+    return net.is_sane()
 
 
 if __name__ == "__main__":
     main()
 
-    graph = G.get_disnet()
+    G = net.get_disnet()
