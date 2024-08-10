dc04da2abaa8766d87afd71ea79a5206bb339a26
caiwei <caiwei@stanford.edu>
2024-08-10 08:19:42 -0700

    Specify option 1 or 2 in test_frank_read_src_pydis_exadis.py in command line (default: 1); remove separate file ..._opt2.py

--- examples/02_frank_read_src/test_frank_read_src_pydis_exadis.py
+++ examples/02_frank_read_src/test_frank_read_src_pydis_exadis.py
@@ -15,7 +15,7 @@ from pydis import VisualizeNetwork, SimulateNetwork
 try:
     import pyexadis
     from pyexadis_base import ExaDisNet
-    from pyexadis_base import CalForce as ExaDiS_CalForce, MobilityLaw as ExaDiS_MobilityLaw
+    from pyexadis_base import CalForce as ExaDiS_CalForce, MobilityLaw as ExaDiS_MobilityLaw, Topology as ExaDiS_Topology
     from pyexadis_base import TimeIntegration as ExaDiS_TimeIntegration, Collision as ExaDiS_Collision, Remesh as ExaDiS_Remesh
 except ImportError:
     raise ImportError('Cannot import pyexadis')
@@ -60,10 +60,10 @@ def main():
     exadis_mobility  = ExaDiS_MobilityLaw(mobility_law='SimpleGlide', state=state)
 
     pydis_timeint    = PyDiS_TimeIntegration(integrator='EulerForward', dt=1.0e-8, state=state)
-    exadis_timeint   = ExaDiS_TimeIntegration(integrator='EulerForward', dt=1.0e-8, state=state)
+    exadis_timeint   = ExaDiS_TimeIntegration(integrator='Trapezoid', dt=1.0e-8, state=state, force=pydis_calforce, mobility=exadis_mobility)
 
     pydis_topology   = PyDiS_Topology(split_mode='MaxDiss', state=state, force=exadis_calforce, mobility=pydis_mobility)
-    exadis_topology  = None
+    exadis_topology  = ExaDiS_Topology(topology_mode='TopologySerial', state=state, force=pydis_calforce, mobility=exadis_mobility)
 
     pydis_collision  = PyDiS_Collision(collision_mode='Proximity', state=state, nbrlist=nbrlist)
     exadis_collision = ExaDiS_Collision(collision_mode='Retroactive', state=state)
@@ -71,13 +71,26 @@ def main():
     pydis_remesh     = PyDiS_Remesh(remesh_rule='LengthBased', state=state)
     exadis_remesh    = ExaDiS_Remesh(remesh_rule='LengthBased', state=state)
 
+    option = sys.argv[1] if len(sys.argv) > 1 else "1"
+    print(f"  set up sim with option {option}")
 
-    sim = SimulateNetwork(calforce=exadis_calforce, mobility=pydis_mobility, timeint=pydis_timeint,
-                          topology=pydis_topology, collision=exadis_collision, remesh=exadis_remesh, vis=vis,
-                          state=state, max_step=200, loading_mode="stress",
-                          applied_stress=np.array([0.0, 0.0, 0.0, 0.0, -4.0e8, 0.0]),
-                          print_freq=10, plot_freq=10, plot_pause_seconds=0.01,
-                          write_freq=10, write_dir='output', save_state=False)
+    if option == "1":
+        sim = SimulateNetwork(calforce=exadis_calforce, mobility=pydis_mobility, timeint=pydis_timeint,
+                              topology=pydis_topology, collision=exadis_collision, remesh=exadis_remesh, vis=vis,
+                              state=state, max_step=200, loading_mode="stress",
+                              applied_stress=np.array([0.0, 0.0, 0.0, 0.0, -4.0e8, 0.0]),
+                              print_freq=10, plot_freq=10, plot_pause_seconds=0.01,
+                              write_freq=10, write_dir='output', save_state=False)
+    elif option == "2":
+        sim = SimulateNetwork(calforce=exadis_calforce, mobility=pydis_mobility, timeint=exadis_timeint,
+                              topology=exadis_topology, collision=exadis_collision, remesh=exadis_remesh, vis=vis,
+                              state=state, max_step=300, loading_mode="stress",
+                              applied_stress=np.array([0.0, 0.0, 0.0, 0.0, -4.0e8, 0.0]),
+                              print_freq=10, plot_freq=10, plot_pause_seconds=0.01,
+                              write_freq=10, write_dir='output', save_state=False)
+    else:
+        raise ValueError("option needs to be 1 or 2")
+
     sim.run(net, state)
 
     return net.is_sane()
