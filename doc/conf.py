439a70f109a20dc3f405a00502ca058f79e251fa
Eliana Krakovsky <144193466+ekrakov@users.noreply.github.com>
2024-09-13 02:02:09 -0600

    Update conf.py add logo credits with link

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,8 @@ import sys
 
 project = 'OpenDiS'
 copyright = '2024, OpenDiS'
-author = 'OpenDis ~ logo by Jonathan Lin'
+author = 'OpenDis | logo by <a href="https://jlin1212.github.io/" target="_blank">Jonathan Lin</a>'
+
 
 from datetime import datetime
 year = datetime.now().year
