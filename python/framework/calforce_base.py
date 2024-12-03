ac588c2306047c449f3f1be162416f4193d7ee6c
Nicolas Bertin <bertin1@llnl.gov>
2024-12-03 14:52:18 -0800

    Updated calforce_base framework class

--- python/framework/calforce_base.py
+++ python/framework/calforce_base.py
@@ -1,7 +1,7 @@
 """@package docstring
 CalForce_Base: base class for Calforce
 
-Defines interface for the CalForce module
+Defines the interface for the CalForce module
 """
 
 from abc import ABC, abstractmethod 
@@ -13,23 +13,23 @@ Tag = Tuple[int, int]
 class CalForce_Base(ABC):
     """CalForce_Base: base class for CalForce
 
-    Defines interface for the CalForce module
+    Defines the interface for the CalForce module
     """
 
     @abstractmethod
-    def NodeForce(self, DM: DisNetManager, state: dict, pre_compute: bool=True) -> dict:
-        """NodeForce: return nodal forces in a dictionary
+    def NodeForce(self, N: DisNetManager, state: dict, pre_compute: bool=True) -> dict:
+        """NodeForce: compute all nodal forces and store them in the state dictionary
         """
         pass
 
     @abstractmethod
-    def PreCompute(self, DM: DisNetManager, state: dict) -> dict:
+    def PreCompute(self, N: DisNetManager, state: dict) -> dict:
         """PreCompute: pre-compute some data for force calculation
         """
         pass
 
     @abstractmethod
-    def OneNodeForce(self, DM: DisNetManager, state: dict, tag: Tag, update_state: bool=True) -> dict:
-        """OneNodeForce: compute force calculation on one node
+    def OneNodeForce(self, N: DisNetManager, state: dict, tag: Tag, update_state: bool=True) -> np.array:
+        """OneNodeForce: compute and return the force on one node specified by its tag
         """
         pass
