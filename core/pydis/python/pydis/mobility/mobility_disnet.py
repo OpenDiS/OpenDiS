c7557d973fb4ecab6acb66a8d8e6a6c70560ebff
Nicolas Bertin <bertin1@llnl.gov>
2025-01-16 12:01:47 -0800

    Implemented OneNodeMobility function in pydis mobility

--- core/pydis/python/pydis/mobility/mobility_disnet.py
+++ core/pydis/python/pydis/mobility/mobility_disnet.py
@@ -8,21 +8,22 @@ import numpy as np
 from ..disnet import DisNet, DisNode
 from framework.disnet_manager import DisNetManager
 
-# To do:
-# 1. Implement OneNodeMobility in pydis, to use exadis_topology with pydis_mobility
+from typing import Tuple
+Tag = Tuple[int, int]
+
 
 class MobilityLaw:
     """MobilityLaw: class for mobility laws
-
     """
     def __init__(self, state: dict={}, mobility_law: str='Relax', vmax: float=1e9) -> None:
         self.mobility_law = mobility_law
         self.mob = state.get("mob", 1.0)
         self.vmax = vmax
 
-        self.Mobility_Functions = {
-            'Relax': self.Mobility_Relax,
-            'SimpleGlide': self.Mobility_SimpleGlide }
+        self.NodeMobility_Functions = {
+            'Relax': self.NodeMobility_Relax,
+            'SimpleGlide': self.NodeMobility_SimpleGlide
+        }
         
     def Mobility(self, DM: DisNetManager, state: dict) -> dict:
         """Mobility: calculate node velocity according to mobility law function
@@ -32,25 +33,48 @@ class MobilityLaw:
             DisNet.convert_nodeforce_array_to_dict(state)
 
         nodeforce_dict = state["nodeforce_dict"]
-        vel_dict = self.Mobility_Functions[self.mobility_law](G, nodeforce_dict)
+        vel_dict = nodeforce_dict.copy()
+        for tag in G.all_nodes_tags():
+            f = vel_dict[tag].copy()
+            vel_dict[tag] = self.NodeMobility_Functions[self.mobility_law](G, tag, f)
         state["vel_dict"] = vel_dict
 
         # prepare nodeforces and nodeforce_tags arrays for compatibility with exadis
         state = DisNet.convert_nodevel_dict_to_array(state)
         return state
-
-    def Mobility_Relax(self, G: DisNet, nodeforce_dict: dict) -> dict:
-        """Mobility_Relax: node velocity equal node force
+        
+    def OneNodeMobility(self, DM: DisNetManager, state: dict, tag: Tag, f: np.array, update_state: bool=True) -> np.array:
+        """OneNodeMobility: compute and return the mobility of one node specified by its tag
         """
-        vel_dict = nodeforce_dict.copy()
+        G = DM.get_disnet(DisNet)
+        v = self.NodeMobility_Functions[self.mobility_law](G, tag, f)
+        # update velocity dictionary if needed
+        if update_state:
+            if "nodevels" in state and "nodeveltags" in state:
+                nodeveltags = state["nodeveltags"]
+                ind = np.where((nodeveltags[:,0]==tag[0])&(nodeveltags[:,1]==tag[1]))[0]
+                if ind.size == 1:
+                    state["nodevels"][ind[0]] = v
+                else:
+                    state["nodevels"] = np.vstack((state["nodevels"], v))
+                    state["nodeveltags"] = np.vstack((state["nodeveltags"], tag))
+            else:
+                state["nodevels"] = np.array([v])
+                state["nodeveltags"] = np.array([tag])
+        return v
+    
+    def NodeMobility_Relax(self, G: DisNet, tag: Tag, f: np.array) -> np.array:
+        """NodeMobility_Relax: node velocity equal node force
+        """
+        vel = 1.0*f
         # set velocity of pinned nodes to zero
         for tag in G.all_nodes_tags():
-            if G.nodes[tag].get('constraint') == DisNode.Constraints.PINNED_NODE:
-                vel_dict[tag] = np.zeros(3)
-        return vel_dict
-
+            if G.nodes(tag).constraint == DisNode.Constraints.PINNED_NODE:
+                vel = np.zeros(3)
+        return vel
+        
     @staticmethod
-    def ortho_vel_glide_planes(vel: np.ndarray, normals: np.ndarray, eps_normal = 1.0e-10) -> np.ndarray:
+    def ortho_vel_glide_planes(vel: np.ndarray, normals: np.ndarray, eps_normal=1.0e-10) -> np.ndarray:
         """ortho_vel_glide_planes: project velocity onto glide planes
         """
         # first orthogonalize glide plane normals among themselves
@@ -65,31 +89,28 @@ class MobilityLaw:
         # then orthogonalize velocity with glide plane normals
         vel -= np.dot( np.dot(vel, normals.T), normals )
         return vel
-
-    def Mobility_SimpleGlide(self, G: DisNet, nodeforce_dict: dict) -> dict:
-        """Mobility_SimpleGlide: node velocity equal node force divided by sum of arm length / 2
+    
+    def NodeMobility_SimpleGlide(self, G: DisNet, tag: Tag, f: np.array) -> np.array:
+        """NodeMobility_SimpleGlide: node velocity equal node force divided by sum of arm length / 2
            To do: add glide constraints
         """
-        vel_dict = nodeforce_dict.copy()
-        for tag in G.all_nodes_tags():
-            node1 = G.nodes(tag)
-            # set velocity of pinned nodes to zero
-            if node1.constraint == DisNode.Constraints.PINNED_NODE:
-                vel_dict[tag] = np.zeros(3)
-            else:
-                R1 = node1.R.copy()
-                Lsum = 0.0
-                for nbr_tag, node2 in G.neighbors_dict(tag).items():
-                    R2 = node2.R.copy()
-                    # apply PBC
-                    R2 = G.cell.closest_image(Rref=R1, R=R2)
-                    Lsum += np.linalg.norm(R2-R1)
-                vel = vel_dict[tag] / (Lsum/2.0) * self.mob
-                normals = np.array([edge.plane_normal for edge in G.neighbor_segments_dict(tag).values()])
-                #print("Mobility_SimpleGlide: tag = %s, vel = %s, normals = %s"%(tag, str(vel), str(normals)))
-                vel = self.ortho_vel_glide_planes(vel, normals)
-                vel_norm = np.linalg.norm(vel)
-                if vel_norm > self.vmax:
-                    vel *= self.vmax / vel_norm
-                vel_dict[tag] = vel
-        return vel_dict
+        node1 = G.nodes(tag)
+        # set velocity of pinned nodes to zero
+        if node1.constraint == DisNode.Constraints.PINNED_NODE:
+            vel = np.zeros(3)
+        else:
+            R1 = node1.R.copy()
+            Lsum = 0.0
+            for nbr_tag, node2 in G.neighbors_dict(tag).items():
+                R2 = node2.R.copy()
+                # apply PBC
+                R2 = G.cell.closest_image(Rref=R1, R=R2)
+                Lsum += np.linalg.norm(R2-R1)
+            vel = f / (Lsum/2.0) * self.mob
+            normals = np.array([edge.plane_normal for edge in G.neighbor_segments_dict(tag).values()])
+            #print("Mobility_SimpleGlide: tag = %s, vel = %s, normals = %s"%(tag, str(vel), str(normals)))
+            vel = self.ortho_vel_glide_planes(vel, normals)
+            vel_norm = np.linalg.norm(vel)
+            if vel_norm > self.vmax:
+                vel *= self.vmax / vel_norm
+        return vel
