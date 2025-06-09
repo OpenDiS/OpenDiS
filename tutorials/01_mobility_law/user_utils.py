76ff5e894c3919bd06e6537549678bdcbce2fa0c
caiwei <caiwei@stanford.edu>
2025-06-09 15:31:29 -0700

    Make user defined My_MobilityLaw extend MobilityLaw_Base from framework

--- tutorials/01_mobility_law/user_utils.py
+++ tutorials/01_mobility_law/user_utils.py
@@ -11,7 +11,9 @@ from pydis import Collision, Remesh, VisualizeNetwork, SimulateNetwork
 
 from pydis.disnet import Tag
 
-class My_MobilityLaw(MobilityLaw):
+from framework.mobility_base import MobilityLaw_Base
+
+class My_MobilityLaw(MobilityLaw_Base):
     """MobilityLaw: class for mobility laws
     """
     def __init__(self, state: dict={}, mobility_law: str='Relax', vmax: float=1e9) -> None:
@@ -22,10 +24,65 @@ class My_MobilityLaw(MobilityLaw):
         self.mob_screw = state.get("mob_screw", 0.1)
 
         self.NodeMobility_Functions = {
-            'Relax': self.NodeMobility_Relax,
-            'SimpleGlide': self.NodeMobility_SimpleGlide,
             'BCCGlide': self.NodeMobility_BCCGlide
         }
+
+    # Implement method required by MobilityLaw_Base
+    def Mobility(self, DM: DisNetManager, state: dict) -> dict:
+        """Mobility: compute all nodal velocities and store them in the state dictionary
+        """
+        G = DM.get_disnet(DisNet)
+        if "nodeforces" in state and "nodeforcetags" in state:
+            DisNet.convert_nodeforce_array_to_dict(state)
+
+        nodeforce_dict = state["nodeforce_dict"]
+        vel_dict = nodeforce_dict.copy()
+        for tag in G.all_nodes_tags():
+            f = vel_dict[tag].copy()
+            vel_dict[tag] = self.NodeMobility_Functions[self.mobility_law](G, tag, f)
+        state["vel_dict"] = vel_dict
+
+        # prepare nodeforces and nodeforce_tags arrays for compatibility with exadis
+        state = DisNet.convert_nodevel_dict_to_array(state)
+        return state
+
+    # Implement method required by MobilityLaw_Base
+    def OneNodeMobility(self, N: DisNetManager, state: dict, tag: Tag, f: np.array, update_state: bool=True) -> np.array:
+        """OneNodeMobility: compute and return the mobility of one node specified by its tag
+        """
+        G = DM.get_disnet(DisNet)
+        v = self.NodeMobility_Functions[self.mobility_law](G, tag, f)
+        # update velocity dictionary if needed
+        if update_state:
+            if "nodevels" in state and "nodeveltags" in state:
+                nodeveltags = state["nodeveltags"]
+                ind = np.where((nodeveltags[:,0]==tag[0])&(nodeveltags[:,1]==tag[1]))[0]
+                if ind.size == 1:
+                    state["nodevels"][ind[0]] = v
+                else:
+                    state["nodevels"] = np.vstack((state["nodevels"], v))
+                    state["nodeveltags"] = np.vstack((state["nodeveltags"], tag))
+            else:
+                state["nodevels"] = np.array([v])
+                state["nodeveltags"] = np.array([tag])
+        return v
+
+    @staticmethod
+    def ortho_vel_glide_planes(vel: np.ndarray, normals: np.ndarray, eps_normal=1.0e-10) -> np.ndarray:
+        """ortho_vel_glide_planes: project velocity onto glide planes
+        """
+        # first orthogonalize glide plane normals among themselves
+        for i in range(normals.shape[0]):
+            for j in range(i):
+                normals[i] -= np.dot(normals[i], normals[j]) * normals[j]
+            if np.linalg.norm(normals[i]) < eps_normal:
+                normals[i] = np.array([0.0, 0.0, 0.0])
+            else:
+                normals[i] /= np.linalg.norm(normals[i])
+
+        # then orthogonalize velocity with glide plane normals
+        vel -= np.dot( np.dot(vel, normals.T), normals )
+        return vel
 
     def NodeMobility_BCCGlide(self, G: DisNet, tag: Tag, f: np.array) -> np.array:
         """NodeMobility_BCCGlide: node velocity equal local mobility times node force divided by sum of arm length / 2
